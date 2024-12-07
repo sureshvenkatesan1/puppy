e9ff347ec4dd88d9fe56330cc68bb630ee9a8726
Suresh Venkatesan <sureshv@jfrog.com>
2024-12-06 23:39:47 -0800

    AU-0 - Before tag v1.1.0

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "fmt"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func From11() {
+	fmt.Println("I am from version 1.1.0")
 }
